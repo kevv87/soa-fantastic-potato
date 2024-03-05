2c5076c95dbb83d0d61c11858d28e84683d77c28
Jess <jessicaes2000@gmail.com>
2024-03-05 16:55:38 -0600

    [Update] Minor changes to syntax

--- API1_test/src/interfaces.js
+++ API1_test/src/interfaces.js
@@ -16,22 +16,21 @@ class RecomendacionNotFoundError extends Error {
 }
 
 class CategoryNotFoundError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = this.constructor.name;
-        this.stack = (new Error()).stack;
-        this.httpsCode = 404;
-    }
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+    this.stack = new Error().stack;
+    this.httpsCode = 404;
+  }
 }
 
 class OpenAIError extends Error {
-    constructor(message, httpsCode) {
-        super(message);
-        this.httpsCode = httpsCode;
-        this.name = this.constructor.name;
-        this.stack = (new Error()).stack;
-        
-    }
+  constructor(message, httpsCode) {
+    super(message);
+    this.httpsCode = httpsCode;
+    this.name = this.constructor.name;
+    this.stack = new Error().stack;
+  }
 }
 
 class OpenAiIface {
@@ -69,60 +68,53 @@ class OpenAiIface {
       ` platilloPrincipal, bebida, postre, y como valor una unica palabra` +
       ` con tu recomendacion`;
 
-        // TODO: Aqui tambien el API puede tirarnos errores, como:
-        //   - Unathorized - Creditos gastados, etc...
-        // Aquí lo que hice fue nada más redirigir los errores que tiraba OpenAI para que no se cayera el servicio
-        // y no tener que hacer 1 por 1 porque no tengo un key con créditos para probarlo xd
-        try{
-            const completion = await openai.chat.completions.create({
-                messages: [
-                {
-                    role: "user",
-                    content: prompt,
-                },
-                ],
-                model: "gpt-3.5-turbo",
-            });
-            console.log(completion.choices[0]["status"]);
-            return {
-                input: prompt,
-                // TODO: Otro error, el JSON de respuesta puede no ser parseable
-                // Esto no tengo como probarlo porque no tengo creditos :c
-                recomendacion: JSON.parse(completion.choices[0]["message"]["content"])
-                
-            }
-        }catch(error){
-            console.log(error.status);
-            console.log(error.message);
-            throw new OpenAIError(
-                `OpenAI failed to do something: ${error.message}`, error.status);
-        }
-        
-        
-        
+    // TODO: Aqui tambien el API puede tirarnos errores, como:
+    //   - Unathorized - Creditos gastados, etc...
+    // Aquí lo que hice fue nada más redirigir los errores que tiraba OpenAI para que no se cayera el servicio
+    // y no tener que hacer 1 por 1 porque no tengo un key con créditos para probarlo xd
+    try {
+      const completion = await openai.chat.completions.create({
+        messages: [
+          {
+            role: 'user',
+            content: prompt,
+          },
+        ],
+        model: 'gpt-3.5-turbo',
+      });
+      console.log(completion.choices[0]['status']);
+      return {
+        input: prompt,
+        // TODO: Otro error, el JSON de respuesta puede no ser parseable
+        // Esto no tengo como probarlo porque no tengo creditos :c
+        recomendacion: JSON.parse(completion.choices[0]['message']['content']),
+      };
+    } catch (error) {
+      console.log(error.status);
+      console.log(error.message);
+      throw new OpenAIError(`OpenAI failed to do something: ${error.message}`, error.status);
     }
+  }
 
-    async getRecomendacion(platilloPrincipal, bebida, postre, res) {
-        try{
-            const open_ai_res = await this._ask_openai(platilloPrincipal, bebida, postre);
-            console.log(open_ai_res);
-            res.json(open_ai_res);
-        }catch (error) {
-            if (error instanceof OpenAIError) {
-                console.log(
-                    "OpenAPI couldnt send a response ");
-                res.status(error.httpsCode);
-                res.json({"message":error.message});
-                
-                res.end();
-            } else {
-                res.status(500);
-                res.end();
-                throw error;
-            }
-            
-        }
+  async getRecomendacion(platilloPrincipal, bebida, postre, res) {
+    try {
+      const open_ai_res = await this._ask_openai(platilloPrincipal, bebida, postre);
+      console.log(open_ai_res);
+      res.json(open_ai_res);
+    } catch (error) {
+      if (error instanceof OpenAIError) {
+        console.log('OpenAPI couldnt send a response ');
+        res.status(error.httpsCode);
+        res.json({ message: error.message });
+
+        res.end();
+      } else {
+        res.status(500);
+        res.end();
+        throw error;
+      }
     }
+  }
 }
 
 class PeerIface {
@@ -153,20 +145,16 @@ class DatabaseIface {
     this.defaultResponses = defaultResponses;
   }
 
-    // Obtains responses from the defaultResponses file
-    _getPredefinedResponse(category) {
-        // Obtains a response depending on the category
-        const responses = this.defaultResponses[category];
-        if (!responses)
-            throw new CategoryNotFoundError(
-                `Not found category: ${category}`);
-        if (responses.length == 0)
-            throw new RecomendacionNotFoundError(
-                `There are no recomendations for ${category}`);
-        // Random selection from the data
-        const randomIndex = Math.floor(Math.random() * responses.length);
-        return responses[randomIndex];
-    }
+  // Obtains responses from the defaultResponses file
+  _getPredefinedResponse(category) {
+    // Obtains a response depending on the category
+    const responses = this.defaultResponses[category];
+    if (!responses) throw new CategoryNotFoundError(`Not found category: ${category}`);
+    if (responses.length == 0) throw new RecomendacionNotFoundError(`There are no recomendations for ${category}`);
+    // Random selection from the data
+    const randomIndex = Math.floor(Math.random() * responses.length);
+    return responses[randomIndex];
+  }
 
   async getRecomendacion(platilloPrincipal, bebida, postre, res) {
     console.log('Attempting to get recomendacion');
@@ -179,37 +167,33 @@ class DatabaseIface {
 
       const respuestaPostre = postre ? postre : this._getPredefinedResponse('postre');
 
-            res.json({
-                respuestaPrincipal,
-                respuestaPostre,
-                respuestaBebida,
-            });
-            res.status(200);
-        } catch (error) {
-            if (error instanceof RecomendacionNotFoundError) {
-                console.log(
-                    "Recomendacion not found, telling requester we couldn't " + 
-                    " find the resource");
-                res.status(error.httpsCode);
-                res.json({"message":error.message});
-                console.error(error.message);
-                
-                res.end();
-            }if (error instanceof CategoryNotFoundError) {
-                console.log(
-                    "Category not found, telling requester we couldn't " + 
-                    " find the resource");
-                res.status(error.httpsCode);
-                res.json({"message":error.message});
-                console.error(error.message);
-                
-                res.end();
-            } else {
-                res.status(500);
-                res.end();
-                throw error;
-            }
-        }
+      res.json({
+        respuestaPrincipal,
+        respuestaPostre,
+        respuestaBebida,
+      });
+      res.status(200);
+    } catch (error) {
+      if (error instanceof RecomendacionNotFoundError) {
+        console.log("Recomendacion not found, telling requester we couldn't " + ' find the resource');
+        res.status(error.httpsCode);
+        res.json({ message: error.message });
+        console.error(error.message);
+
+        res.end();
+      }
+      if (error instanceof CategoryNotFoundError) {
+        console.log("Category not found, telling requester we couldn't " + ' find the resource');
+        res.status(error.httpsCode);
+        res.json({ message: error.message });
+        console.error(error.message);
+
+        res.end();
+      } else {
+        res.status(500);
+        res.end();
+        throw error;
+      }
     }
   }
 }
--- API1_test/src/routing.js
+++ API1_test/src/routing.js
@@ -5,14 +5,8 @@ const { RecommenderController } = require('./controllers');
 recomender = new RecommenderController();
 
 // OpenAPI get
-<<<<<<< HEAD
 router.get('/OpenAI', recomender.get_open_ai_response);
 router.get('/DefaultResponses', recomender.get_database_response);
 router.get('/Endpoint', async (req, res) => res.send(await recomender.get_peer_response(req, res)));
-=======
-router.get("/OpenAI", recomender.get_open_ai_response);
-router.get("/DefaultResponses", recomender.get_database_response);
-router.get("/Endpoint", recomender.get_peer_response);
->>>>>>> origin/error_handling
 
 module.exports = router;
